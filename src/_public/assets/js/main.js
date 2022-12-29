08c2f65b06407c724ebaf81e8544c11a7a6ce0d7
Quoc-Vi <quocvi.agl@gmail.com>
2022-12-29 16:29:09 +0700

    INDEX PC DONE !

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,6 +1,5 @@
 $('.c-design__slider').slick({
-    autoplay:false,
-    autoplaySpeed: 2000,
+    autoplay:true,
     accessibility: true,
     prevArrow: '<button type="button" class="slick-arrow slick-prev"></button>',
     nextArrow: '<button type="button" class="slick-arrow slick-next"></button>',
