bd7a449d6cf19f53bffea3b9e3804355b70ea852
Quoc-Vi <quocvi.agl@gmail.com>
2023-01-03 17:31:04 +0700

    Completed Tablet, 60% SP

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,5 +1,5 @@
 $('.c-design__slider').slick({
-    autoplay:true,
+    autoplay:false,
     accessibility: true,
     prevArrow: '<button type="button" class="slick-arrow slick-prev"></button>',
     nextArrow: '<button type="button" class="slick-arrow slick-next"></button>',
